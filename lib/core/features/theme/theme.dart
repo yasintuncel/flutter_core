75e7e07f71e2050007e72bd739ad50ca67f5b2a9
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-12-28 11:48:10 +0300

    fixes

--- lib/core/features/theme/theme.dart
+++ lib/core/features/theme/theme.dart
@@ -1,45 +1,25 @@
 import 'package:flutter/material.dart';
 
 import '../../constants/colors.dart';
+import '../../extensions/color.dart';
 
-ThemeData getThemeData() {
-  return ThemeData.light().copyWith(
-    primaryColor: mainColor,
-    indicatorColor: mainColor,
-    splashColor: mainColor,
-    primaryColorDark: mainColorDark,
-    highlightColor: mainColor,
-    checkboxTheme: CheckboxThemeData(
-      fillColor: MaterialStateProperty.all<Color>(mainColor),
-    ),
-    colorScheme: const ColorScheme.light().copyWith(
-      secondary: mainColor,
-    ),
-    appBarTheme: const AppBarTheme(
-      backgroundColor: mainColor,
-      centerTitle: true,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
-      ),
-    ),
-    buttonTheme: const ButtonThemeData(
-      buttonColor: fumeColor,
-      splashColor: mainColor,
-    ),
-    progressIndicatorTheme: ProgressIndicatorThemeData(color: mainColor),
-    textButtonTheme: TextButtonThemeData(
-      style: ButtonStyle(
-        foregroundColor: MaterialStateProperty.all<Color>(mainColor),
-        overlayColor: MaterialStateProperty.resolveWith<Color?>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.hovered)) return mainColor.withOpacity(0.04);
-            if (states.contains(MaterialState.focused) || states.contains(MaterialState.pressed)) return mainColor.withOpacity(0.12);
-            return null; // Defer to the widget's default.
-          },
-        ),
-      ),
-    ),
-  );
-}
+// 0xffd00000
+MaterialColor mainMaterialColor = MaterialColor(
+  mainColor.toHex(),
+  <int, Color>{
+    50: mainColor.withOpacity(.1),
+    100: mainColor.withOpacity(.2),
+    200: mainColor.withOpacity(.3),
+    300: mainColor.withOpacity(.4),
+    400: mainColor.withOpacity(.5),
+    500: mainColor.withOpacity(.6),
+    600: mainColor.withOpacity(.7),
+    700: mainColor.withOpacity(.8),
+    800: mainColor.withOpacity(.9),
+    900: mainColor.withOpacity(1),
+  },
+);
+
+ThemeData mainTheme = ThemeData(
+  primarySwatch: mainMaterialColor,
+);
