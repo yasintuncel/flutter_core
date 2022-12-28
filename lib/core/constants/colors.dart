4e399cd41a7ed1f43a9f5c8fa01601e33337d6a5
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-12-28 12:27:56 +0300

    theme color moved to colors file

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-// main colors
-const Color mainColor = Color(0xffd00000);
+import '../extensions/color.dart';
+
+// main color // 0xffd00000
+const Color mainColor = Color.fromARGB(255, 10, 178, 255);
+
 const Color mainColorDark = Color(0xff9d0208);
 
 // text colors
@@ -19,3 +22,19 @@ const Color fumeColorLigth = Color(0xff777777);
 const Color textLigth = Colors.green;
 
 const Color borderColor = Color(0xff444444);
+
+MaterialColor mainMaterialColor = MaterialColor(
+  mainColor.toHex(),
+  <int, Color>{
+    50: mainColor.withOpacity(.1),
+    100: mainColor.withOpacity(.2),
+    200: mainColor.withOpacity(.3),
+    300: mainColor.withOpacity(.4),
+    400: mainColor.withOpacity(.5),
+    500: mainColor.withOpacity(.6),
+    600: mainColor.withOpacity(.7),
+    700: mainColor.withOpacity(.8),
+    800: mainColor.withOpacity(.9),
+    900: mainColor.withOpacity(1),
+  },
+);
--- lib/core/features/theme/theme.dart
+++ lib/core/features/theme/theme.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../constants/colors.dart';
-import '../../extensions/color.dart';
-
-// 0xffd00000
-MaterialColor mainMaterialColor = MaterialColor(
-  mainColor.toHex(),
-  <int, Color>{
-    50: mainColor.withOpacity(.1),
-    100: mainColor.withOpacity(.2),
-    200: mainColor.withOpacity(.3),
-    300: mainColor.withOpacity(.4),
-    400: mainColor.withOpacity(.5),
-    500: mainColor.withOpacity(.6),
-    600: mainColor.withOpacity(.7),
-    700: mainColor.withOpacity(.8),
-    800: mainColor.withOpacity(.9),
-    900: mainColor.withOpacity(1),
-  },
-);
 
 ThemeData mainTheme = ThemeData(
   primarySwatch: mainMaterialColor,
