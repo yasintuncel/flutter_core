5b8f3e13788bea5b6c1017169b3b0361f02544a6
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2023-01-03 14:16:02 +0300

    Update colors.dart

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -7,20 +7,6 @@ const Color mainColor = Color.fromARGB(255, 10, 178, 255);
 
 const Color mainColorDark = Color(0xff9d0208);
 
-// text colors
-const Color mainTextColor = Color(0xffebebeb);
-
-const Color mainTextColorLight = Color(0xffffdd00);
-const Color mainTextColorDark = Color(0xffcccccc);
-
-const Color mainWhiteColor = Colors.white;
-
-const Color whiteLight = Color(0xfff4f4f4);
-const Color fumeColor = Color(0xff444444);
-const Color fumeColorLigth = Color(0xff777777);
-
-const Color textLigth = Colors.green;
-
 const Color borderColor = Color(0xff444444);
 
 MaterialColor mainMaterialColor = MaterialColor(
